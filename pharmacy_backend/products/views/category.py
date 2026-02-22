e978dce15d3de396976e007346baaaadbb76c8f9
DocMe <meosamuede@gmail.com>
2026-02-22 14:08:32 +0100

    Categories: align permissions with product access (allow create for users who can add products)

--- pharmacy_backend/products/views/category.py
+++ pharmacy_backend/products/views/category.py
@@ -3,6 +3,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
+from permissions.roles import IsPharmacistOrAdmin
 from products.models import Category
 from products.serializers.category import CategorySerializer
 
@@ -11,16 +12,10 @@ class CategoryViewSet(viewsets.ModelViewSet):
     """
     Category API
 
-    Responsibilities:
-    - Create categories
-    - List categories
-    - Retrieve category details
-
-    Used by:
-    - Product creation
-    - Filtering / grouping
+    Policy (aligned with Product management):
+    - Anyone who can access inventory/product management can also create categories.
     """
 
-    queryset = Category.objects.all()
+    queryset = Category.objects.all().order_by("name")
     serializer_class = CategorySerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsPharmacistOrAdmin]
